8fcae27127d787a35fa63bdd1cff8b342633bb0a
Girish Parulekar <113516103+Girish-GP@users.noreply.github.com>
2025-04-27 23:06:34 +0530

    Delete Mega-Project:JARVIS directory

--- Mega-Project:JARVIS/musicLibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    'you': 'https://www.youtube.com/watch?v=0dLezZIZOAQ',
-    'blue': 'https://www.youtube.com/watch?v=IpFX2vq8HKw',
-    'smile':'https://www.youtube.com/watch?v=kPa7bsKwL-c'
-    
-}
